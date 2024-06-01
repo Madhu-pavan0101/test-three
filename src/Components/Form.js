eae5425aed5c05444dc51370ac2e82ec50874caa
Madhu-pavan0101 <imsiddhu78@gmail.com>
2024-06-01 15:57:08 +0530

    first commit

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -7,25 +7,24 @@ function Form() {
   const[Password,setPassword]=useState('')
 const submitHandler=(e)=>{
   e.preventDeafult()
-  console.log(name,phonenumber,Password)
+  console.log('data',name,phonenumber,Password)
 }
 const obj={
   name:name,
   phonenumber:phonenumber,
   Password:Password
 }
-console.log(obj)
+console.log(obj,'name')
 
   return (
     <div className='form-matine'>
       <form  onSubmit={submitHandler}>
-      <TextInput placeholder='enter the name' type='name' label='Name Of User' variant='filled' value={name} onChange={(e)=>setName(e.target.value)}></TextInput>
+    <TextInput placeholder='enter the name' type='name' label='Name Of User' variant='filled' value={name} onChange={(e)=>setName(e.target.value)}></TextInput>
     <TextInput placeholder='enter the phone number' label="User Phone Number" variant='filled' value={phonenumber} onChange={(e)=>setPhoneNumber(e.target.value)}></TextInput>
     <PasswordInput label="Enter Correct Password" value={Password} onChange={(e)=>setPassword(e.target.value)}></PasswordInput>
-   <Button>Submit</Button>
+    <Button>Submit</Button>
     </form>
     </div>
   )
 }
-
 export default Form
